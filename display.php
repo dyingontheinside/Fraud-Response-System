787a5fb2a731b5397911f5236e4cfcf0f8782ab7
dyingontheinside <dc4cat@gmail.com>
2018-04-09 12:42:56 -0500

    Add files via upload
    table now displays, no data shows for some reason

---
+++ display.php
@@ -0,0 +1,65 @@
+<?php  
+	require_once("final_functions.php"); 
+	require_once("session.php");
+?>
+<?php
+	new_header(); 
+	$mysqli= db_connection();
+ 	echo "<style> table, th, td {border: 1px solid black;}</style>";
+?>
+<?php
+	$acct=$_GET['acct'];
+	$result = mysqli_query($mysqli,"SELECT * FROM employee where Account_Number = ".(int)$acct."");
+	echo "<body>";
+	echo "<table>";
+	echo "
+		<tr>
+			<th>Account Number</th>
+			<td>".$result['Account_Number']."</td>
+		</tr>
+		<tr>
+			<th>Full Name</th>
+			<td>".$result['Full_Name']."</td>
+		</tr>
+		<tr>
+			<th>First Name</th>
+			<td>".$result['First_Name']."</td>
+		</tr>
+		<tr>
+			<th>Middle Name</th>
+			<td>".$result['Middle_Name']."</td>
+		</tr>
+		<tr>
+			<th>Last Name</th>
+			<td>".$result['Last_Name']."</td>
+		</tr>
+		<tr>
+			<th>Suffix</th>
+			<td>".$result['Suffix']."</td>
+		</tr>
+		<tr>
+			<th rowspan='3'>Address</th>
+			<td>".$result['Address_1']."</td>
+			<td>".$result['Address_2']."</td>
+			<td>".$result['Address_3']."</td>
+		</tr>
+		<tr>
+			<th>City</th>
+			<td>".$result['City']."</td>
+		</tr>
+		<tr>
+			<th>State</th>
+			<td>".$result['State']."</td>
+		</tr>
+		<tr>
+			<th>Zip Code</th>
+			<td>".$result['Zip_Code']."</td>
+		</tr>
+		<tr>
+			<th rowspan='2'>Phone Number</th>
+			<td>".$result['Phone_Number1']."</td>
+			<td>".$result['Phone_Number2']."</td>
+		</tr>";
+	echo "</table>"; 
+	echo "</body>";
+?>
