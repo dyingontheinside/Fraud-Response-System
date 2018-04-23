056ea9e9ac59df8597b1a445a017263c34a34c89
dyingontheinside <dc4cat@gmail.com>
2018-04-22 21:48:33 -0500

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,28 @@
+<?php  
+	require_once("final_functions.php"); 
+	require_once("session.php");
+?>
+<?php
+	new_header(); 
+	$mysqli= db_connection();
+?>
+<?php
+	$value=$_POST['option'];
+	if(isset($_POST['option'])){
+		if($value==1){
+			redirect_to('upload.php');
+		}
+		else if($value==2){
+			redirect_to('editUser.php');
+		}
+		else if($value==0){
+			redirect_to('employee.php');
+		}
+	}
+?>
+ <form action='admin.php' name='value' method='POST'>
+	<input type="radio" name="option" value="0" checked> View Customers<br>
+	<input type="radio" name="option" value="1"> Upload File<br>
+	<input type="radio" name="option" value="2"> Edit Access<br>
+	<input type="submit" name="submit" value="Enter" class="button" />
+</form> 
